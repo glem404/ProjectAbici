7ce3432a0cc9ce70a34a62b27753a1d5d9ee5bb2
Laverde22 <laverdemc77@gmail.com>
2023-11-30 22:45:58 -0500

    Mas Avances css

--- resources/views/administrador/dashboardadmin.blade.php
+++ resources/views/administrador/dashboardadmin.blade.php
@@ -2,74 +2,55 @@
 <link rel="stylesheet" href="{{asset('css/dashboardadmin.css')}}">
 @section('content')
 <div class="content">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-4">
-        <div class="card mb-3 aling-items-center">
-          <div class="card-body d-flex align-items-center justify-content-center">
-            <img src="{{ asset('img/gestionpedido.jpeg') }}" alt="">
-          </div>
-          <div class="card-footer text-center">
-            <a href="#" class="btn btn-primary">Pedidos Pendientes</a>
-          </div>
+    <div class="container-fluid">
+        <!-- Banner grande con imagen y botón -->
+        <div class="row mb-4 ">
+            <div class="col-md-12">
+                <div class="card">
+                    <img src="{{ asset('img/abiciverde.png') }}" class="card-img-top" alt="Banner Image">
+                    <div class="card-body text-center">
+                        <a href="#" class="btn btn-lg btn-primary">pedidos</a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
 
-      <div class="col-md-4">
-        <div class="card mb-3 aling-items-center">
-          <div class="card-body d-flex align-items-center justify-content-center">
-            <img src="{{ asset('img/gestionpedido.jpeg') }}" alt="">
-          </div>
-          <div class="card-footer text-center">
-            <a href="#" class="btn btn-primary">Domiciliarios</a>
-          </div>
-        </div>
-      </div>
+        <!-- Navegación o divs con botones para acceder a otras opciones -->
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mb-3 align-items-center">
+                    <div class="card-body d-flex align-items-center justify-content-center">
+                        <img src="{{ asset('img/gestionpedido.jpeg') }}" alt="">
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="#" class="btn btn-primary">Pedidos Pendientes</a>
+                    </div>
+                </div>
+            </div>
 
-      <div class="col-md-4">
-        <div class="card mb-3 aling-items-center">
-          <div class="card-body d-flex align-items-center justify-content-center">
-            <img src="{{ asset('img/gestionpedido.jpeg') }}" alt="">
-          </div>
-          <div class="card-footer text-center">
-            <a href="#" class="btn btn-primary">Clientes</a>
-          </div>
-        </div>
-      </div>
+            <div class="col-md-4">
+                <div class="card mb-3 align-items-center">
+                    <div class="card-body d-flex align-items-center justify-content-center">
+                        <img src="{{ asset('img/gestionpedido.jpeg') }}" alt="">
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="#" class="btn btn-primary">Domiciliarios</a>
+                    </div>
+                </div>
+            </div>
 
-      <div class="col-md-4">
-        <div class="card mb-3 aling-items-center">
-          <div class="card-body d-flex align-items-center justify-content-center">
-            <img src="{{ asset('img/gestionpedido.jpeg') }}" alt="">
-          </div>
-          <div class="card-footer text-center">
-            <a href="#" class="btn btn-primary">Clientes</a>
-          </div>
+            <div class="col-md-4">
+                <div class="card mb-3 align-items-center">
+                    <div class="card-body d-flex align-items-center justify-content-center">
+                        <img src="{{ asset('img/gestionpedido.jpeg') }}" alt="">
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="#" class="btn btn-primary">Clientes</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Agrega más columnas según las opciones del administrador -->
         </div>
-      </div>
-
-      <div class="col-md-4">
-        <div class="card mb-3 aling-items-center">
-          <div class="card-body d-flex align-items-center justify-content-center">
-            <img src="{{ asset('img/gestionpedido.jpeg') }}" alt="">
-          </div>
-          <div class="card-footer text-center">
-            <a href="#" class="btn btn-primary">Clientes</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-4">
-        <div class="card mb-3 aling-items-center">
-          <div class="card-body d-flex align-items-center justify-content-center">
-            <img src="{{ asset('img/gestionpedido.jpeg') }}" alt="">
-          </div>
-          <div class="card-footer text-center">
-            <a href="#" class="btn btn-primary">Clientes</a>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
 </div>
 @endsection
--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="sidebar" data-color="orange" data-background-color="white" data-image="{{ asset('img/sidebar-1.jpg') }}">
+<div class="sidebar" data-color="green" data-background-color="white" data-image="{{ asset('img/sidebar-1.jpg')}}">
   <!--
       Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
 
@@ -20,7 +20,7 @@
       <li class="nav-item {{ ($activePage == 'profile' || $activePage == 'user-management') ? ' active' : '' }}">
         <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
           <i><img style="width:25px" src="{{ asset('img/laravel.svg') }}"></i>
-          <p>{{ __('Laravel Examples') }}
+          <p>{{ __('Opciones De Perfil') }}
             <b class="caret"></b>
           </p>
         </a>
